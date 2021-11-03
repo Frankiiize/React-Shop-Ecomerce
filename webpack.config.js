b402aa20414929868ac41336f7d64e7af5ebd8d5
Francisco <frankiiize.gmail.com>
2021-11-03 14:34:53 -0300

    [FIX] public path 2

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.resolve(__dirname,'build'),
     filename: 'bundle.js',
     hashFunction: "xxhash64",
-    publicPath: '/React-Shop-Ecomerce',
+    publicPath: '/React-Shop-Ecomerce/',
   },
   module: {
     rules: [
