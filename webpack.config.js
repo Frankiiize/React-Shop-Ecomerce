3301401cd99eed80a909ebfa6653712f93d6a41d
Francisco <frankiiize.gmail.com>
2022-01-24 19:40:23 -0300

    [CONFIG] optimizacion

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -13,7 +15,6 @@ module.exports = {
     publicPath: '/',
     clean:true,
   },
-  devtool: 'eval-source-map',
   module: {
     rules: [
 			{
@@ -57,5 +58,12 @@ module.exports = {
       filename: '[name].css'
     }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ]
+  }
  
 }
